457a85e2cda8052b33d631d551661440b4194200
GIRI NARASIMHA T <46864302+GiriNarasimhaT@users.noreply.github.com>
2023-02-24 17:59:25 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const {Schema,model}=mongoose;
 
 const UserSchema = new Schema({
     profilePicture:String,
-    username:{type: String, required:true},
+    username:{type: String, required:true, unique:true},
     email:{type: String, required:true, unique:true},
     password:{type:String, required:true},
     postcount: { type: Number, default: 0 },
